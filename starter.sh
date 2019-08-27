d5b3fea7e3826f3242f170a4841dc5fa19341fa7
Kostiantyn Sologubov <ksologubov@luxoft.com>
2019-08-27 15:07:01 +0300

    replaced python http server with nginx

--- starter.sh
+++ starter.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sed -i "s/localhost/${HOSTNAME}/g" sdl_core/sdl_preloaded_pt.json
+if [[ $1 = "stop" ]]
+then
+  docker-compose down; exit
+fi
 
-docker build -t sdl_core:latest sdl_core
-
-docker build -t generic_hmi:latest generic_hmi
-
-docker-compose up -d
+sed -i "s/localhost/${HOSTNAME}/g" sdl_core/sdl_preloaded_pt.json \
+&& docker build -t sdl_core:latest sdl_core \
+&& docker build -t generic_hmi:latest generic_hmi \
+&& docker-compose up -d
