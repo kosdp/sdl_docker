390260fb93256d99c25ca196fe29e3c491293671
Kostiantyn Sologubov <ksologubov@luxoft.com>
2019-08-28 19:18:31 +0300

    added ATF image

--- starter.sh
+++ starter.sh
@@ -8,5 +8,6 @@ fi
 sed "s/localhost/${HOSTNAME}/g" sdl_core/sdl_preloaded_pt.json > sdl_core/sdl_preloaded_pt.json.new \
 && docker build -t sdl_core:latest sdl_core \
 && docker build -t generic_hmi:latest generic_hmi \
+&& docker build -t sdl_atf:latest sdl_atf \
 && rm sdl_core/sdl_preloaded_pt.json.new \
 && docker-compose up -d
